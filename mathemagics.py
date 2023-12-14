376046b17df118a2da5a94d86265fb6a708edf82
pandysudhan <pandysudhan@gmail.com>
2023-12-14 14:47:09 -0600

    ssl self

--- mathemagics.py
+++ mathemagics.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", debug=False, port=5000, ssl_context='adhoc')
+    app.run(host = "0.0.0.0", debug=False, port=5000, ssl_context=('cert.pem', 'key.pem'))
